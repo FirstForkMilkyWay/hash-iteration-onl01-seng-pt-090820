403df470828c4c83eb70afa12022ddc504756d34
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-18 12:58:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,6 +4,7 @@
 #   "Amanda" => 27
 # }
 
+require 'pry'
 def happy_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
