0c07c132b94514306113282328dd744a33896f41
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-18 12:54:46 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -29,4 +29,11 @@ describe "#age_appropriate_birthday" do
 end
 
 def age_appropriate_birthday(birthday_kids)
-  
+      birthday_kids.each do |kids_name, age|
+    if age <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else 
+      puts ""
+  end
+end
+
